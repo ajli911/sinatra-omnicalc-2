10837d5830b2118e5f0b089fcc05c36bd685be7b
AJ Li <164284583+ajli911@users.noreply.github.com>
2024-04-09 09:00:47 +0000

    finished

--- app.rb
+++ app.rb
@@ -2,17 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  redirect("/add")
+  erb(:add_form)
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
+
   erb(:add_result)
 end
 
@@ -24,14 +26,21 @@ get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @result = @second_num - @first_num
+
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @second_num * @first_num
+
   erb(:mult_result)
 end
 
@@ -40,5 +49,9 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
   erb(:div_result)
 end
